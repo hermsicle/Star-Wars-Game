02b9b62d148eb45e53c604cb5fa5b74e837ced92
Herman Liu <hermanliu95@gmail.com>
2019-10-10 13:21:46 -0700

    styled the css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
+//create a click function for every character
+//have the character clicked upon move to the your character section
+//then on the second click, have the second character go to the defender section
+//when click attack button, substract HP lost from both your character and defender
+//create a random number when attacking
+//Each character has 3 attributes, health points, attack power, and counter attack power
+//Each time your character attacks, their attack power increases by base attack power
+//example, if base attack is 6, each attack will increase by 6 (12,18,24 and so on)
+//The enemy character only has a counter attack, which never changes.
+//The health points , attack power, and counter attack power must be different for each character
 
 
 
 
-    
 })
