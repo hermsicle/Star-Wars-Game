920292d8cb207e4ef81450090d5ff8bd0ca7a9c2
Herman Liu <hermanliu95@gmail.com>
2019-10-12 16:44:45 -0700

    click function moved to my character section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,28 +9,45 @@
 //The enemy character only has a counter attack, which never changes.
 //The health points , attack power, and counter attack power must be different for each character
 $(document).ready(function () {
+    //New Approach for creating constructor
+    function Char(name, healthPoints, attackPower, counterAttackPower) {
+        this.name = name;
+        this.healthPoints = healthPoints;
+        this.attackPower = attackPower;
+        this.counterAttackPower = counterAttackPower;
+    }
+    var char1 = new Char('Obi-Wan-Kenobi', '120', '6', '15');
+    console.log(char1);
 
+    var char2 = new Char('Luke Skywalker', '100', '8', '12');
+    console.log(char2);
 
+    var char3 = new Char('Darth Sidious', '150', '5', '18');
+    console.log(char3);
 
-//New Approach for creating constructor
-function Char(name, healthPoints, attackPower, counterAttackPower){
-    this.name = name;
-    this. healthPoints = healthPoints;
-    this.attackPower = attackPower;
-    this.counterAttackPower = counterAttackPower;
-}
-var char1 = new Char ('Obi-Wan-Kenobi', '120', '6', '15');
-console.log(char1);
+    var char4 = new Char('Darth Maul', '100', '8', '12');
+    console.log(char4);
 
-var char2 = new Char ('Luke Skywalker', '100', '8', '12');
-console.log(char2);
+    //Click function for selecting characters
+    $('.obi-wan').on('click', function () {
+        $('.obi-wan').remove();
+        $('.your-character').append('<img id="obi-wan" src="./assets/images/obiWan.png">');
+    });
 
-var char3 = new Char ('Darth Sidious' , '150' , '5', '18');
-console.log(char3);
+    $('.luke').on('click', function () {
+        $('.luke').remove();
+        $('.your-character').append('<img id="luke" src="assets/images/luke.png">');
+    });
 
-var char4 = new Char ('Darth Maul', '100', '8', '12');
-console.log(char4);
+    $('.sidious').on('click', function () {
+        $('.sidious').remove();
+        $('.your-character').append('<img id="sidious" src="./assets/images/sidious.png">');
+    });
 
+    $('.maul').on('click', function () {
+        $('.maul').remove();
+        $('.your-character').append('<img id="maul" src="assets/images/maul.png">');
+    });
 
 
 
